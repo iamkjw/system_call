6122fd9c6353082c2e97a1231928fbafdaa3d54c
iamkjw <33914169+iamkjw@users.noreply.github.com>
2019-08-03 10:48:07 -0700

    Add files via upload

--- array_stats.c
+++ array_stats.c
@@ -1,11 +1,14 @@
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
 #include "array_stats.h"
 
 asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long size){
   int i;
-  long local;
-  long min;
-  long max;
-  long sum;
+  long local = 0;
+  long min = LONG_MAX;
+  long max = LONG_MIN;
+  long sum = 0;
 
   //checking for valid input
   if (size <= 0){
@@ -16,11 +19,25 @@ asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long siz
   }
 
   for (i; i < size; i++){
-    if (copy_from_user(&local, data[i])){
+    //validating pointer passed by user
+    if (copy_from_user(&local, data[i], sizeof(long))){
       return -EFAULT;
     }
     else{
-      if(local > )
+      if (local > max){
+        max = local;
+      }
+      if (local < min){
+        min = local;
+      }
+      sum += local;
     }
   }
+  struct array_stats results = {min, max, sum};
+
+  //inserting local results into user pointer
+  if (copy_to_user(stats, &results, sizeof(struct array_stats){
+    return -EFAULT;
+  }
+  return 0;
 }
